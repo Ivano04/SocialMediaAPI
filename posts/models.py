2f6db0f8c59b7c2678125045b67f3451fdf2282b
ACER <ivanocutrignelli@gmail.com>
2025-06-23 15:36:57 +0200

    Creazione modelli 'accounts' per gli utenti e 'posts' per le funzionalità like, commenti e post

---
+++ posts/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.conf import settings
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, related_name="likes", on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
