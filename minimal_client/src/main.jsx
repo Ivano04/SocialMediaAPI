503d446146dca2e8be544fd317c77a41479398cf
ACER <ivanocutrignelli@gmail.com>
2025-07-06 17:28:17 +0200

    commit finale

--- minimal_client/src/main.jsx
+++ minimal_client/src/main.jsx
@@ -9,4 +9,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>,
 );
 
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "http://socialmediaapi-production-5286.up.railway.app:8000";
